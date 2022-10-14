5b5a357661a84b0aa596b9126227d88a9376bb3c
Nicolas Brites <nicolas.britesv@gmail.com>
2022-10-13 22:22:21 -0300

    Agregar archivos de proyecto.

---
+++ ProductoVenta.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PreEntregaCoderHouse
+{
+    public class ProductoVenta
+    {
+        public int Id { get; set; }
+        public int Stock { get; set; }
+        public int IdProducto { get; set; }
+        public int IdVenta { get; set; }
+
+        public ProductoVenta()
+        {
+            Id = 0;
+            Stock = 0;
+            IdProducto = 0;
+            IdVenta = 0;
+        }
+
+        public static List<ProductoVenta> TraerVentas(int idUsuario)
+        {
+            var listaProductoVenta = new List<ProductoVenta>();
+
+            SqlConnectionStringBuilder conecctionbuilder = new();
+            conecctionbuilder.DataSource = "NIKITODEVSS1";
+            conecctionbuilder.InitialCatalog = "SistemaGestion";
+            conecctionbuilder.IntegratedSecurity = true;
+            var cs = conecctionbuilder.ConnectionString;
+
+            using (SqlConnection conecction = new SqlConnection(cs))
+            {
+                conecction.Open();
+                SqlCommand cmd = conecction.CreateCommand();
+                cmd.CommandText = "SELECT pv.Id, pv.Stock ,pv.IdProducto ,pv.IdVenta FROM ProductoVendido pv INNER JOIN Producto p on pv.IdProducto = p.Id WHERE p.IdUsuario = @idUsu";
+
+                var paramIdUsu = new SqlParameter("idUsu", System.Data.SqlDbType.Int);
+                paramIdUsu.Value = idUsuario;
+
+                cmd.Parameters.Add(paramIdUsu);
+
+                var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    var prodven = new ProductoVenta();
+
+                    prodven.Id = Convert.ToInt32(reader.GetValue(0));
+                    prodven.Stock = Convert.ToInt32(reader.GetValue(1));
+                    prodven.IdProducto = Convert.ToInt32(reader.GetValue(2));
+                    prodven.IdVenta = Convert.ToInt32(reader.GetValue(3));
+
+                    listaProductoVenta.Add(prodven);
+                }
+
+                reader.Close();
+
+                return listaProductoVenta;
+            }
+        }
+        public static List<ProductoVenta> TraerProductoVendido(int idUsuario)
+        {
+            var listaProductoVenta = new List<ProductoVenta>();
+
+            SqlConnectionStringBuilder conecctionbuilder = new();
+            conecctionbuilder.DataSource = "NIKITODEVSS1";
+            conecctionbuilder.InitialCatalog = "SistemaGestion";
+            conecctionbuilder.IntegratedSecurity = true;
+            var cs = conecctionbuilder.ConnectionString;
+
+            List<Producto> productos = Producto.TraerProducto(idUsuario);
+
+            foreach (Producto p in productos)
+            {
+
+                using (SqlConnection conecction = new SqlConnection(cs))
+                {
+                    conecction.Open();
+                    SqlCommand cmd = conecction.CreateCommand();
+                    cmd.CommandText = "SELECT * FROM ProductoVendido WHERE idProducto = @idProducto";
+
+                    var paramIdProducto = new SqlParameter("idProducto", System.Data.SqlDbType.Int);
+                    paramIdProducto.Value = p.Id;
+
+                    cmd.Parameters.Add(paramIdProducto);
+
+                    var reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        var prodven = new ProductoVenta();
+
+                        prodven.Id = Convert.ToInt32(reader.GetValue(0));
+                        prodven.Stock = Convert.ToInt32(reader.GetValue(1));
+                        prodven.IdProducto = Convert.ToInt32(reader.GetValue(2));
+                        prodven.IdVenta = Convert.ToInt32(reader.GetValue(3));
+
+                        listaProductoVenta.Add(prodven);
+                    }
+
+                    reader.Close();
+                }
+            }
+
+            return listaProductoVenta;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using PreEntregaCoderHouse;
+
+Console.WriteLine("Ingrese el nombre de usuario:");
+string nombreUsu = Console.ReadLine();
+Console.WriteLine("Ingrese la contraseña:");
+string contra = Console.ReadLine();
+
+Usuario.InicioDeSesion(nombreUsu, contra);
+
+
+Console.WriteLine("Ingrese el Nombre del usuario");
+nombreUsu = Console.ReadLine();
+
+Usuario.TraerUsuario(nombreUsu);
+
+
+Console.WriteLine("Ingrese el ID del usuario");
+int idUsuario = Convert.ToInt32(Console.ReadLine());
+
+Producto.TraerProducto(idUsuario);
+
+
+Console.WriteLine("Ingrese el ID del usuario");
+idUsuario = Convert.ToInt32(Console.ReadLine());
+
+ProductoVenta.TraerVentas(idUsuario);
+
+
+Console.WriteLine("Ingrese el ID del usuario");
+idUsuario = Convert.ToInt32(Console.ReadLine());
+
+ProductoVenta.TraerProductoVendido(idUsuario);
+
+
+
+
+
+
